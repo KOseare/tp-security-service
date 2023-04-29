b8afb77e0b8abb8300fa6b9889f7a8c70dff5cd2
Dario Funes <kdfunes@outlook.com>
2023-04-29 17:45:34 -0300

    Fix monitoreComercio

--- Client/src/negocio/MonitoreoComercio.java
+++ Client/src/negocio/MonitoreoComercio.java
@@ -6,33 +6,12 @@ package negocio;
  */
 public class MonitoreoComercio extends Contratacion {
     
-<<<<<<< HEAD
     public MonitoreoComercio(Domicilio domicilio){
         super(domicilio);
         this.precioDelServicio = 10000.0;
-=======
-    /**
-     * Establece el precio de la contratacion.
-     * 
-     * <b>post:</b>
-     * <ul>
-     * <li>Se establece un precio incial de la contratacion.</li>
-     * <li>Se suman los precios de los servicios adicionales.</li>
-     * <li>Se resta el descuento de la promocion (si corresponde).</li>
-     * </ul>
-     */   
-    @Override
-    public void setPrecio() {
-        this.precio = 8500.0;
-        this.precio += this.obtenerTotalDeServiciosAdicionales();
-        this.precio-=calculaPromo(this.promo);
->>>>>>> 558af84beb14844640af1668a17371e1200d3b35
     }
     
 
-<<<<<<< HEAD
-    
-=======
     /**
      * Calcula el descuento de la promocion aplicable a la contratacion.
      * 
@@ -46,12 +25,13 @@ public class MonitoreoComercio extends Contratacion {
      * <li>Se devuelve el descuento correspondiente a la promocion.</li>
      * </ul>
      * 
-     * @param promocion La promoción aplicable a la contratacion.
+     * @param promocion La promocion aplicable a la contratacion.
      * @return El descuento correspondiente a la promocion.
      */
->>>>>>> 558af84beb14844640af1668a17371e1200d3b35
     @Override
     public double calculaPromo(Promocion promocion) {
+        assert promocion != null : "La promocion es null";
+
         return promocion.PromoComercio(this.precioDelServicio);
     }
     
