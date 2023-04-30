024521c4cba0d18ac41f485afbeb42b17c0adf80
Dario Funes <kdfunes@outlook.com>
2023-04-29 22:22:52 -0300

    Contrato de persona

--- Client/src/negocio/PersonaFisica.java
+++ Client/src/negocio/PersonaFisica.java
@@ -2,20 +2,37 @@ package negocio;
 
 import java.util.ArrayList;
 
+/**
+ * Clase que representa a una persona fisica.
+ */
 public class PersonaFisica extends Persona {
     @Override
     public void generarFactura() {
         // TODO Implement this method
     }
 
+    /**
+     * Calcula los descuentos que recibe en cada una de sus contrataciones.
+     * 
+     * <b>pre:</b>
+     * <ul>
+     * <li>La lista de contratos esta inicializada (no es null).</li>
+     * </ul>
+     * 
+     * @param contratos Lista de contrataciones de la persona fisica.
+     * @return Lista de valores de descuento para cada contrato.
+     */
     @Override
     public ArrayList<Double> recibeDescuento(ArrayList<Contratacion> contratos) {
+        assert contratos != null : "La lista de contratos no esta inicializada";
+        
         ArrayList<Double> valores = new ArrayList<Double>();
         for(Contratacion c : contratos){
                 valores.add(1.0);
         }
         return valores;
     }
+
     @Override
     public String toString(){
         return "Tipo: Persona Fisica";
--- Client/src/negocio/PersonaJuridica.java
+++ Client/src/negocio/PersonaJuridica.java
@@ -2,15 +2,31 @@ package negocio;
 
 import java.util.ArrayList;
 
+/**
+ * Clase que representa a una persona juridica.
+ */
 public class PersonaJuridica extends Persona {
     @Override
     public void generarFactura() {
         // TODO Implement this method
     }
 
+    /**
+     * Calcula los descuentos que recibe en cada una de sus contrataciones.
+     * 
+     * <b>pre:</b>
+     * <ul>
+     * <li>La lista de contratos esta inicializada (no es null).</li>
+     * </ul>
+     * 
+     * @param contratos Lista de contrataciones de la persona juridica.
+     * @return Lista de valores de descuento para cada contrato.
+     */
     @Override
     public ArrayList<Double> recibeDescuento(ArrayList<Contratacion> contratos) {
-       ArrayList<Double> valores = new ArrayList<Double>();
+        assert contratos != null : "La lista de contratos no esta inicializada";
+        
+        ArrayList<Double> valores = new ArrayList<Double>();
         for(int i = 0;i<contratos.size();i++){
             if(i>2){
                 valores.add(0.5);
@@ -20,6 +36,7 @@ public class PersonaJuridica extends Persona {
         }
         return valores;
     }
+
     @Override
     public String toString(){
         return "Tipo: Persona Juridica";
